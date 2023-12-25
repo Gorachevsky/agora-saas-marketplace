d0350ffc6cd0cf5d22dd2467b2c0fc1e7d9aee3c
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-12-25 17:28:42 +0100

    Fixed semantically the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+      <article className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-62xl">
           Your marketplace for high-quality{" "}
           <span className="text-green-600">digital assets</span>.
@@ -14,13 +14,15 @@ export default function Home() {
           Welcome to Agora&apos;s Marketplace. Every asset on out platform is
           verified by out team to ensure our highest quality standards.
         </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+        <section className="flex flex-col sm:flex-row gap-4 mt-6">
           <Link href="/products" className={buttonVariants()}>
             Browse Trending
           </Link>
           <Button variant="ghost">Our quality promise &rarr;</Button>
-        </div>
-      </div>
+        </section>
+      </article>
+
+      {/* TODO: List products */}
     </MaxWidthWrapper>
   );
 }
